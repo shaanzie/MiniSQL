90b2788ae5dd98feca2bec71204b8f9a20190ffe
shaanzie <ishaanlagwankar@gmail.com>
2019-11-21 12:15:05 +0530

    Parsing of all commands done

--- Python Implementation/PseudoTerminal.py
+++ Python Implementation/PseudoTerminal.py
@@ -1,14 +1,61 @@
 import re
 
+def exists(file):
+
+    with open('metastore.txt', 'r') as file:
+
+        lines = file.readlines()
+
+    file.close()
+
+    for line in lines:
+
+        if(file in line):
+
+            return 1
+    
+    return 0
+
 def parse_load(query):
 
-    if(re.search(r"^([a-zA-Z0-9_\-\.]+)\/([a-zA-Z0-9_\-\.]+)\.[csv$]", query[1])):
+    if(re.search(r"^([a-zA-Z0-9_\-\.]+)\/([a-zA-Z0-9_\-\.]+)\.[csv$]", query[1]) and exists(query[1])):
         if(query[2] == 'as'):
             # if(re.search(r"(^\[ (([a-zA-Z0-9_\-\.]+) \: ([a-zA-Z0-9_\-\.]+) (\,) (\s) +) \]\;$)", query[3])):
             for i in query[3][1:-1].split(","):
                 if(re.search(r"^([a-zA-Z0-9_\-\.]+)\:([a-zA-Z0-9_\-\.]+)", i)):
                     return 1
     return 0
+    
+
+def parse_select(query):
+
+    if(re.search(r"(([a-zA-Z0-9_\-\.]+))", query[1]) or query[1] == '*'):
+
+        if(query[2] == 'from'):
+        
+            if(re.search(r"^([a-zA-Z0-9_\-\.]+)\/([a-zA-Z0-9_\-\.]+)\.[csv$]", query[3]) and exists(query[3])):
+        
+                if(query[4] == 'where'):
+
+                    if(re.search(r"([a-zA-Z0-9_\-\.]+)\=([a-zA-Z0-9_\-\.]+)", query[5])):
+                
+                        return 1
+
+                elif(query[3][-1] == ';'):
+
+                    return 1
+    return 0
+
+def parse_delete(query):
+
+    if(re.search(r"^([a-zA-Z0-9_\-\.]+)\/([a-zA-Z0-9_\-\.]+)\.[csv$]", query[1]) and exists(query[1])):
+
+        return 1
+
+    return 0
+
+
+    
 
 def load(query):
 
