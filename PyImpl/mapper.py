f94af259d3c6bd5b0e9340064487e21b6eb385b7
shaanzie <ishaanlagwankar@gmail.com>
2019-11-28 09:08:14 +0530

    Fixed concurrency

--- PyImpl/mapper.py
+++
@@ -1,6 +0,0 @@
-import csv
-import sys
-for line in sys.stdin:
-	values = line.split(',')
-	if values[0] >3.4 :
-		print(line)
--- PyImpl/reducer.py
+++
@@ -1,5 +0,0 @@
-import csv
-import sys
-for line in sys.stdin:
-	values = line.split(',')
-	print(values[0], values[1], values[2], sep = ' ')
